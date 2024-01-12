64d05673b8678b1408443c0534a51165d3eb7c54
Parth Singh <parth198728@outlook.com>
2024-01-12 15:52:35 +0530

    some changes

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,5 +1,6 @@
 import random
 
+# Function to get user input with input validation
 def get_user_input():
     while True:
         user_choice = input("What is your choice [rock, paper, scissor]: ").lower()
@@ -8,13 +9,14 @@ def get_user_input():
         else:
             print("Processing...")
             return user_choice
-        
+
+# Function to determine the winner of the game
 def determine_winner(user_choice, computer_choice):
     print(f"\nUser's Choice: {user_choice.capitalize()} \nComputer's Choice: {computer_choice.capitalize()}\n")
 
     global user_wins, computer_wins, draws
     if user_choice == computer_choice:
-        print("It's a draw, both have same choices.")
+        print("It's a draw, both have the same choices.")
         draws += 1
     elif (
         (user_choice == "rock" and computer_choice == "scissor") or
@@ -22,28 +24,38 @@ def determine_winner(user_choice, computer_choice):
         (user_choice == "paper" and computer_choice == "rock")
     ):
         print("THE USER HAS WON THE GAME.")
-        user_wins += 1 
+        user_wins += 1
     else:
         print("THE COMPUTER HAS WON THE GAME.")
         computer_wins += 1
 
+# Function to ask if the user wants to play again
 def play_again():
     return input("Do you want to play again? [y] for YES and [n] for NO: ").lower() == "y"
 
+# List of possible outcomes for the game
 possible_outcomes = ["rock", "paper", "scissor"]
+
+# Initial scores
 user_wins = 0
 computer_wins = 0
 draws = 0
 
+# Main game loop
 while True:
+    # Get user input and generate computer choice
     user_choice = get_user_input()
     computer_choice = random.choice(possible_outcomes)
+    
+    # Determine the winner and update scores
     determine_winner(user_choice, computer_choice)
 
+    # Ask if the user wants to play again
     if play_again():
         print("Let's play again.")
     else:
         print("Thanks for playing.")
         break
-    
-print(f"User's Score: {user_wins}, Computer's Score: {computer_wins}, Draw: {draws}")
+
+# Display final scores
+print(f"USER'S SCORE: {user_wins}, COMPUTER'S SCORE: {computer_wins}, DRAW: {draws}")
